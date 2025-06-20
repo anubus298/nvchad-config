0a9586b8f73625b7ce48a4f2704eb3350ef32e8e
anubus298 <araristaf@gmail.com>
2025-06-20 20:55:46 +0100

    added prettierd & google java formatter

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,15 +1,32 @@
+local formatters = {
+  ["google-java-format"] = {
+    command = "google-java-format",
+    args = { "-" }, -- reads from stdin
+    stdin = true,
+  },
+}
+
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
+    javascript = { "prettierd" },
+    javascriptreact = { "prettierd" },
+    typescript = { "prettierd" },
+    typescriptreact = { "prettierd" },
+    json = { "prettierd" },
+    java = { "google-java-format" },
+    css = { "prettierd" },
+    html = { "prettierd" },
+    markdown = { "prettierd" },
+    yaml = { "prettierd" },
   },
 
-   format_on_save = {
-     -- These options will be passed to conform.format()
-     timeout_ms = 500,
-     lsp_fallback = true,
-   },
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 30000,
+    lsp_fallback = true,
+  },
+  formatters = formatters,
 }
 
 return options
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -5,7 +5,6 @@ return {
     opts = require "configs.conform",
   },
 
-  -- These are some examples, uncomment them if you want to see them work!
   {
     "neovim/nvim-lspconfig",
     config = function()
@@ -38,12 +37,9 @@ return {
       require("scrollbar").setup()
     end,
   },
-  -- add this to your lua/plugins.lua, lua/plugins/init.lua,  or the file you keep your other plugins:
   {
     "numToStr/Comment.nvim",
-    opts = {
-      -- add any options here
-    },
+    opts = {},
   },
   {
     "lukas-reineke/indent-blankline.nvim",
@@ -60,6 +56,8 @@ return {
     opts = {
       ensure_installed = {
         "vim",
+        "tsx",
+        "json",
         "javascript",
         "java",
         "typescript",
